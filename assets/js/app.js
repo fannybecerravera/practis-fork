c88992067866ea07f82646cff067194834a3900e
Fanny <fannybecerravera@gmail.com>
2018-05-22 11:53:07 -0400

    Subiendo el ejercicio

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+window.onload =funcion () {
+    var btn = document.getElementById("button-change");
+    var txt = document.getElementById("paragraph");
+    var btnDos = document.getElementById ("magic")
+    var txtDos = document.getElementById ("text")
+    
+    btn.addEventListener("click", function {
+     txt.innerHTML = "<h2> Git & Github TE AMAMOS</h2>";
+    })
+
+    btnDos.addEventListener ("click", function(){
+
+    })
+
+
+}
